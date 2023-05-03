de14f66466bdd9d9c8d7a50ce2fb8888f4bd2598
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-05-03 17:27:30 -0400

    saving

--- docs/assets/reset_migrations.bash
+++ docs/assets/reset_migrations.bash
@@ -1,7 +1,7 @@
 rm -R -f ./migrations &&
 pipenv run init &&
-psql -U gitpod -c 'DROP DATABASE example;' || true &&
-psql -U gitpod -c 'CREATE DATABASE example;' &&
-psql -U gitpod -c 'CREATE EXTENSION unaccent;' -d example &&
+psql postgres -c 'DROP DATABASE example;' || true &&
+psql postgres -c 'CREATE DATABASE example;' &&
+psql postgres -c 'CREATE EXTENSION unaccent;' -d example &&
 pipenv run migrate &&
 pipenv run upgrade
